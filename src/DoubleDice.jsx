04a620e5d375294d0af17245998abc22e7984912
Affan Hakak <affanshowkat@gmail.com>
2024-07-17 13:29:54 +0530

    added conditional header to you win

--- src/DoubleDice.jsx
+++ src/DoubleDice.jsx
@@ -1,10 +1,10 @@
 export default function DoubleDice() {
   const num1 = Math.floor(Math.random() * 3 + 1);
   const num2 = Math.floor(Math.random() * 3 + 1);
-  const result = num1 === num2 ? "You Win Tiger!!!" : "Awww Sheepie :(";
+
   return (
     <>
-      <h1>{result}</h1>
+      {num1 === num2 ? <h3>You Win</h3> : null}
       <h2>Die 1 value : {num1}</h2>
       <h2>Die 2 value : {num2}</h2>
     </>
